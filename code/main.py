b37d0d081e5db1a19bd6e51d423c9b052540a6b9
Alejandro Robles <alejandro.robles@altruisconsulting.com>
2019-02-23 18:00:44 -0800

    Adding code files

---
+++ code/main.py
@@ -0,0 +1,25 @@
+from helper import *
+from solution import *
+
+
+#Use for testing the training and testing processes of a model
+def train_test_a_model(modelname, train_data, train_label, test_data, test_label, max_iter, learning_rate):
+	'''
+	you should try various number of max_iter and learning_rate
+	'''
+
+
+def test_logistic_regression():
+
+	'''
+	you should try various number of max_iter and learning_rate
+	'''
+
+def test_thirdorder_logistic_regression():
+	pass
+
+
+
+if __name__ == '__main__':
+	test_logistic_regression()
+	test_thirdorder_logistic_regression()
